1b0440328b763528829963ef84f3f9ae60df3c59
Joshua Reibert <joshua.reibert@dlr.de>
2022-07-11 13:11:58 +0200

    Implement mean temp chart

--- src/App.svelte
+++ src/App.svelte
@@ -5,17 +5,22 @@
   import { selected } from "./lib/stores/stations";
   import StationsMap from "./lib/StationsMap.svelte";
   import StationsTable from "./lib/StationsTable.svelte";
-  import LineChart from "./lib/LineChart.svelte";
+  import TemperatureChart from "./lib/TemperatureChart.svelte";
 
   import * as d3 from "d3";
 
   $: stationsData = d3.csvParse(csv, (d) => ({
     id: d.station_id,
     date: new Date(d.date),
-    tempMin: +d.temperature_air_min_200,
-    tempMean: +d.temperature_air_mean_200,
-    tempMax: +d.temperature_air_max_200,
+    temperature_air_min_200: +d.temperature_air_min_200,
+    temperature_air_mean_200: +d.temperature_air_mean_200,
+    temperature_air_max_200: +d.temperature_air_max_200,
   }));
+
+  $: temperatureDomain = [
+    d3.min(stationsData, (d) => d.temperature_air_min_200),
+    d3.max(stationsData, (d) => d.temperature_air_max_200),
+  ];
 
   function getStationTimeSeries(id) {
     return stationsData.filter((d) => d.id === id);
@@ -26,10 +31,10 @@
   <div class="h-full grow">
     <StationsMap {stations} />
   </div>
-  <div class="flex flex-col bg-slate-700 w-3/5 text-white">
+  <div class="flex flex-col w-3/5 text-white">
     <StationsTable stations={stations.features.map((feat) => feat.properties)} />
     {#if $selected}
-      <LineChart data={getStationTimeSeries($selected)} />
+      <TemperatureChart data={getStationTimeSeries($selected)} domain={temperatureDomain} />
     {/if}
   </div>
 </main>
---
+++ src/lib/TemperatureChart.svelte
@@ -0,0 +1,80 @@
+<script>
+  import * as d3 from "d3";
+  export let data = [];
+  export let domain = [-20, 40];
+  export let dateFormat = d3.timeFormat("%b %y");
+
+  let width;
+  let height;
+  let marginRight = 40;
+
+  $: xScale = d3
+    .scaleUtc()
+    .domain(d3.extent(data, (d) => d.date))
+    .range([0, width - marginRight]);
+  $: yScale = d3.scaleLinear().domain(domain).range([height, 0]);
+  $: line = d3
+    .line()
+    .curve(d3.curveLinear)
+    .x((d) => xScale(d.date))
+    .y((d) => yScale(d.temperature_air_mean_200));
+  $: xTicks = xScale.ticks(width / 100);
+  $: yTicks = yScale.ticks(3);
+</script>
+
+<div id="container" class="w-full min-h-fit" bind:clientWidth={width} bind:clientHeight={height}>
+  <svg class="w-full">
+    <g id="ticks-x">
+      {#each xTicks as value (value)}
+        <g class="tick" transform="translate({xScale(value)},0)">
+          <line class="tick" y1={0} y2={height} />
+          <text y={yScale.range()[0]} dx="0.2em" dy="-0.4em" fill="white">
+            {dateFormat(value)}
+          </text>
+        </g>
+      {/each}
+    </g>
+
+    <g id="ticks-y">
+      {#each yTicks as value (value)}
+        <g class="tick" transform="translate(0,{yScale(value)})">
+          <line class="tick" x1={0} x2={width} />
+          <text x={width} text-anchor="end" dx="-0.2em" dy="-0.2em" fill="white">
+            {value}
+          </text>
+        </g>
+      {/each}
+    </g>
+
+    <path
+      id="line"
+      d={line(data)}
+      fill="none"
+      stroke="white"
+      stroke-width={1.5}
+      stroke-linecap="round"
+    />
+  </svg>
+</div>
+
+<style lang="postcss">
+  #container {
+    @apply bg-slate-700;
+  }
+
+  svg {
+    aspect-ratio: 16 / 9;
+  }
+
+  #line {
+    stroke: theme("colors.slate.200");
+  }
+
+  .tick line {
+    stroke: theme("colors.slate.600");
+  }
+
+  .tick text {
+    fill: theme("colors.slate.500");
+  }
+</style>
