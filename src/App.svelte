e839cea9902b8d1dc556fe68b3a502d69f4eb25a
Joshua Reibert <joshua.reibert@dlr.de>
2022-07-11 09:20:26 +0200

    Load weather data

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,10 @@
   import StationsMap from "./lib/StationsMap.svelte";
   import StationsTable from "./lib/StationsTable.svelte";
   import stations from "./assets/stations.geo.json";
+  import csv from "./assets/data.csv?raw";
+  import * as d3 from "d3";
+
+  $: data = d3.csvParse(csv, d3.autoType);
 </script>
 
 <main class="flex flex-row w-screen h-screen gap-2 bg-slate-900">
